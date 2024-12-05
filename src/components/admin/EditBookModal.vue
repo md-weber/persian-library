61a2cfdbd54e15979aac391644fad45086fb3f21
Max Weber <weber.max.1988@gmail.com>
2024-12-05 20:35:34 +0100

    Fixed a warning that showed that we access the age group before renderer

--- src/components/admin/EditBookModal.vue
+++ src/components/admin/EditBookModal.vue
@@ -98,7 +98,7 @@
                 {{ $t("admin.addBook.form.ageGroup") }}
               </label>
               <input
-                v-model="ageInput"
+                v-model="editedBook.age"
                 type="text"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                 @input="normalizeAgeInput"
